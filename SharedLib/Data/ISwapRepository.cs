9414cb02431c0444b315db7edae7d7b0507330fa
dejfcz <dejfcz@users.noreply.github.com>
2026-04-13 16:11:27 +0100

    rev

--- SharedLib/Data/ISwapRepository.cs
+++ SharedLib/Data/ISwapRepository.cs
@@ -9,7 +9,7 @@ namespace WhalesExchangeBackend.SharedLib.Data;
 internal interface ISwapRepository
 {
     /// <summary>
-    /// Gets swaps identified by their frontend IDs from the databased.
+    /// Gets swaps identified by their frontend IDs from the database.
     /// </summary>
     /// <param name="frontendIds">Frontend IDs of the swaps to retrieve.</param>
     /// <returns>List of the requested swaps. If any of the ID is not found in the database, the corresponding item in the list is set to <c>null</c>.</returns>
