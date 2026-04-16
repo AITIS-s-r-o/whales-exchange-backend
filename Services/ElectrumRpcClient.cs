afa3515eb6f0ef30674675497ca4d87b5a8d83b5
dejfcz <dejfcz@users.noreply.github.com>
2026-04-16 10:14:05 +0100

    rev

--- Services/ElectrumRpcClient.cs
+++ Services/ElectrumRpcClient.cs
@@ -219,7 +219,7 @@ internal class ElectrumRpcClient
     {
         this.log.Debug("*");
 
-        ElectrumGetInfoResponse result = await this.CallAsync<ElectrumGetInfoResponse>(method: "get_submarine_swap_providers", parameters: null, cancellationToken)
+        ElectrumGetInfoResponse result = await this.CallAsync<ElectrumGetInfoResponse>(method: "getinfo", parameters: null, cancellationToken)
             .ConfigureAwait(false);
 
         this.log.Debug($"$='{result}'");
