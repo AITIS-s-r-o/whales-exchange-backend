500c3163589281092b1ccadc2b79c426921fc389
MartyIX <203266+MartyIX@users.noreply.github.com>
2026-04-17 22:40:03 +0200

    ToString

--- Services/ElectrumRpc/ElectrumGetSubmarineSwapProviderResponse.cs
+++ Services/ElectrumRpc/ElectrumGetSubmarineSwapProviderResponse.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Text;
 
 namespace WhalesExchangeBackend.Services.ElectrumRpc;
 
@@ -10,4 +11,26 @@ namespace WhalesExchangeBackend.Services.ElectrumRpc;
 [SuppressMessage("Microsoft.Performance", "CA1812:AvoidUninstantiatedInternalClasses", Justification = "Instantiated by JSON deserializer.")]
 internal class ElectrumGetSubmarineSwapProviderResponse : Dictionary<string, ElectrumSwapProvider>
 {
+    /// <inheritdoc/>
+    public override string ToString()
+    {
+        StringBuilder sb = new();
+        _ = sb.Append('{');
+
+        int i = 0;
+        foreach ((string key, ElectrumSwapProvider provider) in this)
+        {
+            i++;
+            _ = sb.Append('#');
+            _ = sb.Append(i);
+            _ = sb.Append(": ");
+            _ = sb.Append(key);
+            _ = sb.Append("->");
+            _ = sb.Append(provider);
+            _ = sb.Append(';');
+        }
+
+        _ = sb.Append('}');
+        return sb.ToString();
+    }
 }
