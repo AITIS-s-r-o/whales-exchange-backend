86bb0c84c77999905003e6d5597add36ef4a097e
MartyIX <203266+MartyIX@users.noreply.github.com>
2026-04-27 19:02:51 +0200

    cs

--- Services/ElectrumRpc/ElectrumSwapProvider.cs
+++ Services/ElectrumRpc/ElectrumSwapProvider.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.Text.Json.Serialization;
@@ -96,7 +95,7 @@ internal class ElectrumSwapProvider
         return string.Format
         (
             CultureInfo.InvariantCulture,
-            "[{0}={1},{2}={3},{4}={5},{6}={7},{8}={9},{10}={11},{12}={13},{14}=`{15}`,{16}={17},{18}=`{19}`,{20}=`{21}`]",
+            "[{0}={1},{2}={3},{4}={5},{6}={7},{8}={9},{10}={11},{12}={13},{14}=`{15}`,{16}={17},{18}=`{19}`,{20}={21}]",
             nameof(this.PercentageFee), this.PercentageFee,
             nameof(this.MaxAmountForwardSat), this.MaxAmountForwardSat,
             nameof(this.MaxAmountReverseSat), this.MaxAmountReverseSat,
