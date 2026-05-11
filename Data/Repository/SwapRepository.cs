2ff3bcfb43c4c5ccc4c7d5258d0fa3ad9d7350a0
dejfcz <dejfcz@users.noreply.github.com>
2026-05-11 09:23:04 +0100

    Update Data/Repository/SwapRepository.cs
    Co-authored-by: MartyIX <203266+MartyIX@users.noreply.github.com>

--- Data/Repository/SwapRepository.cs
+++ Data/Repository/SwapRepository.cs
@@ -505,7 +505,7 @@ internal class SwapRepository : RepositoryBase, ISwapRepository
                 // error. In case of a reverse swap, the funding on-chain Bitcoin transaction is created by the swap provider, so if it is not created within the expected time,
                 // it is a provider error.
                 //
-                // In case of a forward swap, claiming the funding transcation output is the responsibility of the swap provider, but the fault cannot be assigned to the provider
+                // In case of a forward swap, claiming the funding transaction output is the responsibility of the swap provider, but the fault cannot be assigned to the provider
                 // because it may also be the case that the client did not reveal the preimage by completing the lightning payment, or it could be the case that the provider did
                 // not even initiate this lightning payment. In case of a reverse swap, claiming the funding transcation output is the responsibility of the client, so if it is
                 // not claimed within the expected time, it is a client error.
