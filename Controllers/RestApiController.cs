225eae3dbdb7ca118e62070b0d2ac843d245272e
dejfcz <dejfcz@users.noreply.github.com>
2026-05-16 12:54:26 +0100

    rev

--- Controllers/RestApiController.cs
+++ Controllers/RestApiController.cs
@@ -34,7 +34,7 @@ internal class RestApiController : InternalControllerBase
     private const int MaxReverseSwapClientAddressHistoryLength = 100;
 
     /// <summary>Number of blocks the swap provider wants to have on its side to claim the funding transaction output in forward swaps.</summary>
-    /// <remarks>Plus one here over the swap server implementation is because of the logic in <c>_claim_swap</c> method.</remarks>
+    /// <remarks>Plus one here over the Swap Server implementation is because of the logic in <c>_claim_swap</c> method in the Swap Server implementation.</remarks>
     private const int ForwardSwapServerTimeoutBlocksBuffer = 60 + 1;
 
     /// <summary>Number of blocks after which the forward swap expires.</summary>
